b6600917d9ed32d4e508f63251fb1f0178d5bee2
arbasang <154686862+arbasang@users.noreply.github.com>
2025-01-13 14:21:00 +0900

    캐릭터, 매니저 1시까지 한거

--- TextRPG2/Character.h
+++ TextRPG2/Character.h
@@ -1,36 +1,43 @@
 #pragma once
-
+#include "item.h"
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 class Character
 {
 private:
-    static Character *Instance;
+    static Character* Instance;
     string Name;
     int Level;
     int MaxLevel;
-    double Health;        // 체력
-    double MaxHealth;     // 최대 체력
-    double Attack;        // 공격력
-    double Experience;    // 경험치
+    double Health; // 체력
+    double MaxHealth; // 최대 체력
+    double Attack; // 공격력
+    double Experience; // 경험치
     double MaxExperience; // 최대 경험치
-    double Gold;          // 돈
+    double Gold; // 돈
+    vector<pair<item*, int>> Inventory;
 
     Character(string name);
 
 public:
-    static Character *GetInstance(string name = "");
+    static Character* GetInstance(string name = "");
     string GetName();
     int GetLevel();
+    int SetLevel(int level);
     double GetHealth();
-    double GetAttack();
     void SetHealth(double health);
+    double GetAttack();
     void SetAttack(double attack);
     void TakeDamage(double damage);
     double GetExperience();
     double GetMaxExperience();
     double GetGold();
+    void DisplayStatus();
+    void DisplayInventory();
+    void LevelUp();
+    void UseItem(int index);
 };
