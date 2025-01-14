bf1837cb7bf1f50d706c614d65c01f1008461b61
Cat-Yubi <kimtaewan144@gmail.com>
2025-01-14 11:40:10 +0900

    .

--- TextRPG2/IItem.h
+++ TextRPG2/IItem.h
@@ -14,8 +14,5 @@ public:
 	virtual string GetName() = 0;
 	virtual void Use(class Character *character) = 0;
 
-protected:
-	ITEM_CATEGORY m_eCategory{ ITEM_END };
 };
 
-enum ITEM_CATEGORY { ITEM_POSITION, ITEM_EQUIP, ITEM_END};
